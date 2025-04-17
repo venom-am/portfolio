e9e3fdbcb0a2987b9880aa067b4e4bd067b9bfd2
venom-am <202352302@iiitvadodara.ac.in>
2025-04-18 02:07:15 +0530

    changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,17 @@
-import { div } from "motion/react-client";
-import Image from "next/image";
-import { NavItems, Navbar } from "./components/ui/resizable-navbar";
 import { NavbarDemo } from "./components/Navbar";
-import { StarsBackground } from "./components/ui/stars-background";
-import { ShootingStarsAndStarsBackgroundDemo } from "./components/ShootingStar";
 import { GlowingEffectDemo } from "./components/GlowEffect";
 import { StickyScrollRevealDemo } from "./components/Skills";
-import { TracingBeam } from "./components/ui/tracing-beam";
 import { TimelineDemo } from "./components/timeline";
 import { GetInTouch } from "./components/ui/get-in-touch";
-// import Project from "./components/Project";
 
-const navItems = ["Home", "About", "Contact"];
 export default function Home() {
   return (
     <div className="bg-neutral-900 py-0 my-0">
       <NavbarDemo />
       <GlowingEffectDemo />
-        <StickyScrollRevealDemo />
+      <StickyScrollRevealDemo />
       <TimelineDemo/>
       <GetInTouch />
     </div>
-    
   );
 }
--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { cn } from '@/app/lib/utils';
@@ -11,4 +11,21 @@ interface NavItemsProps {
   className?: string;
   onItemClick?: (e: React.MouseEvent<HTMLAnchorElement>) => void;
 }
+
+export const NavItems: React.FC<NavItemsProps> = ({ items, className, onItemClick }) => {
+  return (
+    <div className={cn("flex items-center space-x-4", className)}>
+      {items.map((item) => (
+        <Link
+          key={item.name}
+          href={item.link}
+          onClick={onItemClick}
+          className="text-sm font-medium transition-colors hover:text-primary"
+        >
+          {item.name}
+        </Link>
+      ))}
+    </div>
+  );
+};
 
